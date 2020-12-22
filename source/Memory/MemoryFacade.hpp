97f42a9faad0b9b16174d8d3185b7c181cb8c9f0
ObjectBerry <MatBre@centrum.sk>
2020-12-22 21:53:04 +0100

    Created Memory Facade

---
+++ source/Memory/MemoryFacade.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Memory {
+	// This is class used as facade between allocator itself and objects that need memory
+	// Too implement own facade , use this class as parent and override methods
+	class MemoryFacade {
+		virtual void* allocateMemory(size_t size) = 0;
+		virtual void* allocatePernament(size_t size) = 0;
+		virtual char* allocateBytes(size_t size) = 0;
+	};
+}
