79fd008918cf9d5d50590f8a179707d6a6dccbeb
ObjectBerry <MatBre@centrum.sk>
2020-12-22 21:36:55 +0100

    Created TestCase

---
+++ source/Unit_Tests/TestCase.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#define DO_CHECK(checkName, calculation) this->doCheck(checkName, calculation, __LINE__)
+namespace Unit_Tests {
+	class TestCase {
+		char* _caseName;
+
+		unsigned char _checks;
+		unsigned char _failures;
+
+	protected:
+		void doCheck(const char* checkName, bool calculation, int line);
+		void printStatus();
+
+		virtual void runTests() = 0;
+
+	public:
+		TestCase(const char* caseName);
+		void start();
+	};
+}
