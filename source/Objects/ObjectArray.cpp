93089285eb5883efeb0e0813391d84d4bb9668cc
ObjectBerry <MatBre@centrum.sk>
2020-12-29 20:21:30 +0100

    Implemented Object Array

--- source/Objects/ObjectArray.cpp
+++ source/Objects/ObjectArray.cpp
@@ -1,1 +1,25 @@
 #include "ObjectArray.hpp"
+
+Objects::ObjectArray::ObjectArray(basicParameter, unsigned short length) : Object(memoryAllocator, objectMap) {
+	this->_length = length;
+	this->_storage = static_cast<Objects::Object**>( memoryAllocator->allocateMemory(sizeof(Objects::Object*) * length));
+	for (unsigned i = 0; i < this->_length; i++) {
+		this->_storage[i] = 0;
+	}
+}
+
+
+Objects::ObjectArray* Objects::ObjectArray::create(basicParameter, unsigned short length) {
+	return new(memoryAllocator) Objects::ObjectArray(memoryAllocator, objectMap, length);
+}
+
+Objects::ObjectArray* Objects::ObjectArray::clone(Memory::MemoryAllocator* memoryAllocator) {
+	Objects::ObjectArray* clonedArray = Objects::ObjectArray::create(memoryAllocator, this->getObjectMap(), this->_length);
+	this->copyValuesInto(clonedArray);
+
+	for (unsigned i = 0; i < this->_length; i++) {
+		clonedArray->_storage[i] = this->at(i);
+	}
+
+	return clonedArray;
+}
--- source/Objects/ObjectArray.hpp
+++ source/Objects/ObjectArray.hpp
@@ -3,7 +3,26 @@
 
 namespace Objects {
 	class ObjectArray : public Objects::Object {
+		unsigned short		_length;
+		Objects::Object**	_storage;
+		
+		ObjectArray(basicParameter, unsigned short length);
 
+	public:
+		static ObjectArray* create(basicParameter, unsigned short length);
+		ObjectArray* clone(Memory::MemoryAllocator* allocator);
 
+	public:
+		inline Objects::Object* at(unsigned short index) { 
+			return this->_storage[index];
+		};
+		inline void				atPut(unsigned short index, Objects::Object* value) {
+			this->_storage[index] = value; 
+		};
+
+		inline unsigned short		getArrayLength() { return this->_length; };
+		inline Objects::Object**	getArrayStorage() { return this->_storage; };
+	public:
+		OBJECT_TYPE(ObjectType::ByteArray);
 	};
 }
