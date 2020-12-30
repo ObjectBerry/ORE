d6e461bc17664abfd8de7cb88bcee4034c98f209
ObjectBerry <MatBre@centrum.sk>
2020-12-30 01:08:26 +0100

    Fixed counter in native allocator

--- source/Memory/NativeAllocator.cpp
+++ source/Memory/NativeAllocator.cpp
@@ -5,8 +5,10 @@
 Memory::NativeAllocator::NativeAllocator(): _allocatedBytes(0), _allocatedMemory(0) {};
 
 void* Memory::NativeAllocator::allocateMemory(size_t size) {
+	this->_allocatedMemory += size;
 	return malloc(size);
 }
 char* Memory::NativeAllocator::allocateBytes(size_t count) {
+	this->_allocatedBytes += count;
 	return static_cast<char*>(malloc(count));
 }
