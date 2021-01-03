7dab93dafc446db683f59ecc13ca88ec0fd69665
ObjectBerry <MatBre@centrum.sk>
2021-01-03 21:21:29 +0100

    Added destructor into native allocator - we now can avoid memory leak

--- source/Memory/NativeAllocator.cpp
+++ source/Memory/NativeAllocator.cpp
@@ -2,13 +2,27 @@
 
 #include "NativeAllocator.hpp"
 
-Memory::NativeAllocator::NativeAllocator(): _allocatedBytes(0), _allocatedMemory(0) {};
+Memory::NativeAllocator::NativeAllocator(unsigned int bufferSize) {
+	this->_allocatedBytes = 0;
+	this->_allocatedMemory = 0;
+
+	this->_bufferSize = bufferSize;
+	this->_buffer = new char[bufferSize];
+	this->_bufferPointer = _buffer;
+};
+Memory::NativeAllocator::~NativeAllocator() {
+	delete[] this->_buffer;
+}
 
 void* Memory::NativeAllocator::allocateMemory(size_t size) {
+	void* pointer = static_cast<void*>(this->_bufferPointer);
 	this->_allocatedMemory += size;
-	return malloc(size);
+	this->_bufferPointer += size;
+	return pointer;
 }
 char* Memory::NativeAllocator::allocateBytes(size_t count) {
-	this->_allocatedBytes += count;
-	return static_cast<char*>(malloc(count));
+	char* bytes = (this->_bufferPointer);
+	this->_allocatedMemory += count;
+	this->_bufferPointer += count;
+	return bytes;
 }
--- source/Memory/NativeAllocator.hpp
+++ source/Memory/NativeAllocator.hpp
@@ -4,14 +4,19 @@
 namespace Memory {
 	/* This is native allocator - it is using malloc function from <stdlib.h>
 	// It is used only during testing as replacement for object space
-	// It also contain memory counter 
+	// It also contain memory counter  
 	*///
 	class NativeAllocator : public MemoryAllocator {
 		unsigned int _allocatedMemory;
 		unsigned int _allocatedBytes;
-	
+		
+		unsigned int	_bufferSize;
+		char*			_buffer;
+		char*			_bufferPointer;
+
 	public:
-		NativeAllocator();
+		NativeAllocator(unsigned int bufferSize);
+		~NativeAllocator();
 		
 		void* allocateMemory(size_t size);
 		char* allocateBytes(size_t count);
