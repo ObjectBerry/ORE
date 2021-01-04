c10075a2c2322119ffc17d788bd6920f2a608497
ObjectBerry <MatBre@centrum.sk>
2021-01-04 18:01:57 +0100

    Created MemoryBuffer

---
+++ source/Memory/MemoryBuffer.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Memory {
+	class MemoryBuffer {
+		unsigned short _bufferSize;
+		unsigned short _bufferUsedBytes;
+		unsigned short _bufferLimit;
+		char* _buffer;
+		char* _bufferPointer;
+
+	public:
+		
+	};
+}
