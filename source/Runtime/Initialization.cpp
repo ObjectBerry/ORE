1a7ed32e7fe90f7ab77b6a31abf5f26b78ce193c
ObjectBerry <MatBre@centrum.sk>
2021-01-21 08:02:35 +0100

    Added Allocators into dependency container

--- source/Runtime/Initialization.cpp
+++ source/Runtime/Initialization.cpp
@@ -1,3 +1,4 @@
+#include "../Memory/BufferAllocator.hpp"
 #include "../Memory/MemoryAllocator.hpp"
 
 #include "../Interpreter/ExecutionEngine.hpp"
@@ -8,7 +9,9 @@
 void Runtime::initializeVM(int argc, char** argv) {
 	Runtime::_dependencyContainer = new DependencyContainer();
 
-	Runtime::_dependencyContainer->_basicAllocator;
+	Runtime::_dependencyContainer->_basicAllocator =		new Memory::BufferAllocator(1024);
+	Runtime::_dependencyContainer->_tenuredAllocator =		new Memory::BufferAllocator(1024);
+	Runtime::_dependencyContainer->_pernamentAllocator =	new Memory::BufferAllocator(1024);
 }
 void Runtime::handleLineArguments(int argc, char** argv) {
 
