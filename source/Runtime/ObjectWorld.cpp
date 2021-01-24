dbd88cc2118b78e75c4ca0bda20704a570e9107b
ObjectBerry <MatBre@centrum.sk>
2021-01-24 19:38:03 +0100

    Implemented ObjectWorld

--- source/Runtime/ObjectWorld.cpp
+++ source/Runtime/ObjectWorld.cpp
@@ -5,6 +5,7 @@
 #include "../Object_Layout/SlotType.hpp"
 
 #include "../Objects/Object.hpp"
+#include "../Objects/ObjectArray.hpp"
 #include "../Objects/ObjectFactory.hpp"
 #include "../Objects/Symbol.hpp"
 
@@ -12,42 +13,73 @@
 
 
 Runtime::ObjectWorld::ObjectWorld(Objects::ObjectFactory* objectFactory) {
+	this->_objectFactory = objectFactory;
 	// initialize object world
-	Object_Layout::ObjectMap* lobbyMap = objectFactory->createObjectMap(3);
-	lobbyMap->setDescription(0, Object_Layout::SlotDescription(
-		objectFactory->createSymbol("Lobby", Objects::SymbolType::AlphaNumerical, 0),
-		Object_Layout::SlotType::NormalSlot
-	));
-	lobbyMap->setDescription(1, Object_Layout::SlotDescription(
-		objectFactory->createSymbol("globals", Objects::SymbolType::AlphaNumerical, 0),
-		Object_Layout::SlotType::ParentSlot 
-	));
-	lobbyMap->setDescription(2, Object_Layout::SlotDescription(
-		objectFactory->createSymbol("traits", Objects::SymbolType::AlphaNumerical, 0),
-		Object_Layout::SlotType::NormalSlot
-	));
-
-
-	this->lobbyObject = lobbyMap->constructObject(objectFactory->getAllocator());
-	this->lobbyObject->setValue(0, this->lobbyObject); // slot Lobby is pointing to object Lobby
-
-
-	Object_Layout::ObjectMap* globalsMap = objectFactory->createObjectMap(8);
-	Object_Layout::ObjectMap* traitsMap = objectFactory->createObjectMap(7);
-
-	// initialize important objects
-	Object_Layout::ObjectMap* parentMap = objectFactory->createObjectMap(1);
-	parentMap->setDescription(0, Object_Layout::SlotDescription(
-		objectFactory->createSymbol("parent", Objects::SymbolType::AlphaNumerical, 0),
-		Object_Layout::SlotType::ParentSlot
-	));
-
-	this->trueObject =		parentMap->constructObject(objectFactory->getAllocator());
-	this->falseObject =		parentMap->constructObject(objectFactory->getAllocator());
-	this->undefinedObject = parentMap->constructObject(objectFactory->getAllocator());
+	const char* lobbyNames[] = { "Lobby", "globals", "traits", "bootstrap" };
 	
+	this->_lobbyObject = this->_objectFactory->createObjectMap(4)->constructObject(this->_objectFactory->getAllocator());
+	this->setSlotNames(this->_lobbyObject->getObjectMap(), 4, lobbyNames);
+
+	unsigned short prototypeCount = 2;
+	this->_globalsObject = this->_objectFactory->createObjectMap(prototypeCount + 3)->constructObject(this->_objectFactory->getAllocator());
+	this->_traitsObject  = this->_objectFactory->createObjectMap(prototypeCount + 2)->constructObject(this->_objectFactory->getAllocator());
+
+	this->_lobbyObject->setValue(0, this->_lobbyObject);
+	this->_lobbyObject->setValue(1, this->_globalsObject);
+	this->_lobbyObject->setValue(2, this->_traitsObject);
 	
 	// initialize prototypes
+	this->createFullPrototype("ByteArray", this->_objectFactory->createByteArray(1));
+	this->createFullPrototype("ObjectArray", this->_objectFactory->createObjectArray(1));
 
 
+
+	// initialize important objects 
+	this->_trueObject		= this->_objectFactory->createObjectMap(1)->constructObject(this->_objectFactory->getAllocator());
+	this->_falseObject		= this->_trueObject->clone(this->_objectFactory->getAllocator());
+	this->_undefinedObject	= this->_trueObject->clone(this->_objectFactory->getAllocator());
+
+	
+}
+
+void Runtime::ObjectWorld::setSlotNames(Object_Layout::ObjectMap* map, unsigned short nameCount, const char* names[]) {
+	if (map->getSlotCount() != nameCount)
+		return;
+
+	for (unsigned i = 0; i < nameCount; i++) {
+		map->setDescription(i, Object_Layout::SlotDescription(
+			this->_objectFactory->createSymbol(names[i], Objects::SymbolType::AlphaNumerical, 0),
+			Object_Layout::SlotType::NormalSlot
+		));
+	}
+}
+
+void Runtime::ObjectWorld::createFullPrototype(const char* name, Objects::Object* prototype) {
+	Objects::Symbol* slotName = this->_objectFactory->createSymbol(name, Objects::SymbolType::AlphaNumerical, 0);
+
+
+	Object_Layout::SlotDescription* slotDesc;
+	Object_Layout::SlotDescription insertedSlot = Object_Layout::SlotDescription( slotName, Object_Layout::SlotType::NormalSlot);
+	
+	for (unsigned i = 0; i < this->_globalsObject->getSlotCount(); i++) {
+		slotDesc = this->_globalsObject->getObjectMap()->getDescription(i);
+		if (not slotDesc->isValid()) {
+			this->_globalsObject->getObjectMap()->setDescription(i, insertedSlot);
+			this->_globalsObject->setValue(i, prototype);
+			return;
+		}
+	}
+
+	Objects::Object* trait = this->_objectFactory->createObjectMap(0)->constructObject(this->_objectFactory->getAllocator());
+
+	prototype->setValue(0, trait);
+
+	for (unsigned i = 0; i < this->_traitsObject->getSlotCount(); i++) {
+		slotDesc = this->_traitsObject->getObjectMap()->getDescription(i);
+		if (not slotDesc->isValid()) {
+			this->_traitsObject->getObjectMap()->setDescription(i, insertedSlot);
+			this->_traitsObject->setValue(i, trait);
+			return;
+		}
+	}
 }
--- source/Runtime/ObjectWorld.hpp
+++ source/Runtime/ObjectWorld.hpp
@@ -8,29 +8,33 @@ namespace Objects {
 
 namespace Runtime {
 	class ObjectWorld {
+		Objects::ObjectFactory* _objectFactory;
+
 		// important objects used by primitives
-		Objects::Object* trueObject;
-		Objects::Object* falseObject;
-		Objects::Object* undefinedObject;
+		Objects::Object* _trueObject;
+		Objects::Object* _falseObject;
+		Objects::Object* _undefinedObject;
 
 		// object world basic structure
-		Objects::Object* lobbyObject;
-		Objects::Object* globalsObject;
-		Objects::Object* traitsObject;
+		Objects::Object* _lobbyObject;
+		Objects::Object* _globalsObject;
+		Objects::Object* _traitsObject;
 
 	public:
 		ObjectWorld(Objects::ObjectFactory* objectFactory);
 	
 	private:
+		void setSlotNames(Object_Layout::ObjectMap* map, unsigned short nameCount, const char* names[]);
+		void createFullPrototype(const char* name, Objects::Object* prototype);
 		
 
 	public:
-		inline Objects::Object* getTrueObject()		{ return this->trueObject; }
-		inline Objects::Object* getFalseObject()	{ return this->falseObject; }
-		inline Objects::Object* getUndefined()		{ return this->undefinedObject; }
+		inline Objects::Object* getTrueObject()		{ return this->_trueObject; }
+		inline Objects::Object* getFalseObject()	{ return this->_falseObject; }
+		inline Objects::Object* getUndefined()		{ return this->_undefinedObject; }
 
-		inline Objects::Object* getLobbyObject()	{ return this->lobbyObject; }
-		inline Objects::Object* getGlobalsObject()	{ return this->globalsObject; }
-		inline Objects::Object* getTraitsObject()	{ return this->traitsObject; }
+		inline Objects::Object* getLobbyObject()	{ return this->_lobbyObject; }
+		inline Objects::Object* getGlobalsObject()	{ return this->_globalsObject; }
+		inline Objects::Object* getTraitsObject()	{ return this->_traitsObject; }
 	};
 }
